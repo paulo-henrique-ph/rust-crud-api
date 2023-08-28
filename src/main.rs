c2015540eb75213e0bf627ebdad4132e87e8c65c
paulo-henrique-ph <paulohgp343@gmail.com>
2023-08-28 04:15:14 -0300

    feature: implemented server stater

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    // Set database
+    if let Err(e) = set_database(){
+        eprintln!("Error setting database: {}", e);
+        std::process::exit(1);
+    }
+
+    //Start server
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    println!("Server started on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                eprintln!("Unable to connect: {}", e);
+                std::process::exit(1);
+            }
+        }
+    }
 }
