a6cf4d12afeaf8760fdec28f5b4499dd85635d73
paulo-henrique-ph <paulohgp343@gmail.com>
2023-09-01 04:15:17 -0300

    feature: error treatment

--- src/error.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, thiserror::Error)]
-pub enum AppError {
-    #[error("postgres pool error: {0}")]
-    PostgresPool(#[from] bb8_postgres::bb8::RunError<tokio_postgres::Error>),
-
-    #[error("postgres error: {0}")]
-    Postgres(#[from] tokio_postgres::Error),
-}
--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,53 +1,35 @@
-use actix_web::{
-    error,
-    http::{header, StatusCode},
-    HttpResponse,
-};
-use derive_more::{Display, Error};
-
 #[derive(Debug, thiserror::Error)]
-pub enum ApplicationError {}
-
-#[derive(Debug, Display, Error)]
 pub enum AppError {
-    #[display(fmt = "Internal Server Error")]
-    InternalServerError,
+    #[error("postgres pool error: {0}")]
+    PostgresPool(#[from] bb8_postgres::bb8::RunError<tokio_postgres::Error>),
 
-    #[display(fmt = "Bad Request: {}", _0)]
-    BadRequest(String),
-
-    #[display(fmt = "Unauthorized")]
-    Unauthorized,
-
-    #[display(fmt = "Forbidden")]
-    Forbidden,
-
-    #[display(fmt = "Not Found: {}", _0)]
-    NotFound(String),
-
-    #[display(fmt = "Conflict: {}", _0)]
-    Conflict(String),
-
-    #[display(fmt = "Unprocessable Entity: {}", _0)]
-    UnprocessableEntity(String),
-
-    #[display(fmt = "Service Unavailable")]
-    ServiceUnavailable,
+    #[error("postgres error: {0}")]
+    Postgres(#[from] tokio_postgres::Error),
 }
 
-impl error::ResponseError for AppError {
-    fn error_response(&self) -> actix_web::HttpResponse {
-        match self {
-            AppError::InternalServerError => HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR),
-            AppError::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
-            AppError::Unauthorized => HttpResponse::Unauthorized().finish(),
-            AppError::Forbidden => HttpResponse::Forbidden().finish(),
-            AppError::NotFound(ref message) => HttpResponse::NotFound().json(message),
-            AppError::Conflict(ref message) => HttpResponse::Conflict().json(message),
-            AppError::UnprocessableEntity(ref message) => {
-                HttpResponse::build(StatusCode::UNPROCESSABLE_ENTITY).json(message)
-            }
-            AppError::ServiceUnavailable => HttpResponse::ServiceUnavailable().finish(),
-        }
-    }
+#[derive(Debug, thiserror::Error)]
+pub enum ApiError {
+    #[error("internal server error")]
+    InternalServerError,
+
+    #[error("bad request: {0}")]
+    BadRequest(String),
+
+    #[error("unauthorized")]
+    Unauthorized,
+
+    #[error("forbidden")]
+    Forbidden,
+
+    #[error("not found: {0}")]
+    NotFound(String),
+
+    #[error("conflict: {0}")]
+    Conflict(String),
+
+    #[error("unprocessable entity: {0}")]
+    UnprocessableEntity(String),
+
+    #[error("service unavailable")]
+    ServiceUnavailable,
 }
